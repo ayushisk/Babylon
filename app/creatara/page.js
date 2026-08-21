b335517e9f8a1d4713efd721b0f970746738715c
malem <malem@malems-Mac-mini.local>
2026-08-21 17:32:30 +0530

    first commit

---
+++ app/creatara/page.js
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useEffect } from "react";
+import * as BABYLON from "@babylonjs/core";
+import "@babylonjs/loaders";
+
+
+export default function Page() {
+  useEffect(() => {
+    const canvas = document.getElementById("renderCanvas");
+
+    const engine = new BABYLON.Engine(canvas, true);
+
+    const createScene = function () {
+      const scene = new BABYLON.Scene(engine);
+
+      scene.createDefaultCameraOrLight(true, false, true);
+
+      BABYLON.SceneLoader.Append(
+        "/models/",
+        "Vintage Car.stl",
+        scene
+      );
+
+      BABYLON.SceneLoader.Append("/models/", "Vintage Car.stl", scene);
+
+      return scene;
+    };
+
+    const scene = createScene();
+
+    engine.runRenderLoop(function () {
+      scene.render();
+    });
+
+    window.addEventListener("resize", () => {
+      engine.resize();
+    });
+
+    return () => {
+      engine.dispose();
+    };
+  }, []);
+
+  return <canvas id="renderCanvas" />;
+}
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      dfdfdf
+    </div>
+  )
+}
+
+export default page
